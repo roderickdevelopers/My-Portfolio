179e00e7189309732cc1342dcac2827847abfba7
Matthew <roderickdevelopers@gmail.com>
2020-02-17 19:40:24 -0500

    Removed old unused code

--- app/router.js
+++ app/router.js
@@ -7,7 +7,5 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  this.route('certificates');
-  this.route('my-info');
-  this.route('extra');
+  this.route('github');
 });
